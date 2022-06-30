6b05d6ab057a05cdc365babb17ff20af4b2a81b4
kenkszii <kenkszii@gmail.com>
2022-06-30 08:00:51 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+ #include <studio.h>
+/**
+ * main - using printf 
+ *
+ * always Return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,/n");
+return (0);
+}
