f922065c0472af2d963f183e69812520d658b2f4
kenkszii <kenkszii@gmail.com>
2022-06-30 07:20:04 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * main -write "c programming with put"
+ * Return: will always return 0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
